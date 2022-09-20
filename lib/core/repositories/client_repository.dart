b9b9e26eacfca7d06cf367cbe402098faceb6a90
fiki2002 <adepitanoluwatosin202@gmail.com>
2022-09-20 01:38:58 +0100

    Made Changes

--- lib/core/repositories/client_repository.dart
+++ lib/core/repositories/client_repository.dart
@@ -1,38 +1,39 @@
 import 'package:dio/dio.dart';
-import 'package:flutter/foundation.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+import 'package:flutter/foundation.dart';
 
 import '../services/connection_service.dart';
-import '../services/http_service.dart';
 
-class ClientRepository {
+
+
+class HttpClientService {
   late Dio dio;
-  late HttpClientService _httpClientService;
 
-  ClientRepository() {
+  HttpClientService() {
     final options = BaseOptions(
       baseUrl: 'https://cryptowatch-server.herokuapp.com',
       receiveDataWhenStatusError: true,
       connectTimeout: 60 * 1000, // 60 seconds
-      receiveTimeout: 60 * 1000,
+      receiveTimeout: 60 * 1000, // 60 seconds
     );
-
     dio = Dio(options);
-
     dio.interceptors.add(
       InterceptorsWrapper(
+        //onRequest
         onRequest: (
           options,
           handler,
         ) async {
+          //Reject request if there is no internet
           if (!await ConnectionStatus.isConnected()) {
             handler.reject(
               DioError(
                   requestOptions: options,
                   error:
-                      'Oops! Looks like you\'re not connected to the internet. Check your internet connection and try again'),
+                      'Oops! Looks like you\'re not connected to the internet. Check your internet connection and try again.'),
             );
           }
+
           handler.next(options);
         },
       ),
